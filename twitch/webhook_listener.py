b92d4f622798e19917f3f3ec5a7ae0c2fb7f8147
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-08-07 07:17:36 -0500

    start twitch part

--- twitch/webhook_listener.py
+++ twitch/webhook_listener.py
@@ -1,37 +1,52 @@
-from flask import request
+from flask import Flask, request
 
 
-@app.route("/", methods=["GET", "POST"])
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
 def parse_request():
     data = request.data
+    print(data)
 
 
-import web
-
-urls = ("/.*", "hooks")
-
-app = web.application(urls, globals())
-
-
-class hooks:
-    def POST(self):
-        data = web.data()
-        print("")
-        print("DATA RECEIVED:")
-        print(data)
-        print("")
-
-        return "OK"
-
-    def GET(self):
-        try:
-            data = web.input()
-            data = data["hub.challenge"]
-            print("Hub challenge: ", data)
-            return data
-        except KeyError:
-            return web.BadRequest
+@app.route("/", methods=["GET"])
+def parse_get_request():
+    try:
+        data = request.data
+        data = data["hub.challenge"]
+        return data
+    except KeyError:
+        return "error"
 
 
 if __name__ == "__main__":
     app.run()
+#
+# urls = ("/.*", "hooks")
+#
+# app = web.application(urls, globals())
+#
+#
+# class hooks:
+#     def POST(self):
+#         data = web.data()
+#         print("")
+#         print("DATA RECEIVED:")
+#         print(data)
+#         print("")
+#
+#         return "OK"
+#
+#     def GET(self):
+#         try:
+#             data = web.input()
+#             data = data["hub.challenge"]
+#             print("Hub challenge: ", data)
+#             return data
+#         except KeyError:
+#             return web.BadRequest
+#
+#
+# if __name__ == "__main__":
+#     app.run()
