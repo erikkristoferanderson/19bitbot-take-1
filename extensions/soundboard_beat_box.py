c4d2a8e4faad9e774a8fb1555a2f1cc8a92acc1e
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-08-06 20:37:51 -0500

    start soundboard beat box

--- extensions/soundboard_beat_box.py
+++ extensions/soundboard_beat_box.py
@@ -1,4 +1,8 @@
+import os
+
 from core.base import CustomClient
+
+import logging
 
 from interactions import (
     Button,
@@ -10,10 +14,26 @@ from interactions import (
     component_callback,
     slash_command,
 )
+from dotenv import load_dotenv
+
+load_dotenv()
+
+guild_id = os.getenv("TEST_SERVER_ID")
 
 
 class SoundboardExtension(Extension):
     bot: CustomClient
+
+    @slash_command(name="soundboard-go", description="make it go", scopes=[guild_id])
+    async def soundboard_go(selfself, ctx: InteractionContext):
+        """Triggers the soundbaord to make a sound"""
+        selected_channel = None
+        for channel in ctx.guild.channels:
+            if channel.name == "General":
+                selected_channel = channel
+        if selected_channel is None:
+            logging.warning("Channel not found")
+        # selected_channel.
 
 
 def setup(bot: CustomClient):
